33a30b9b7a578c69544659ce53d58cbc17fb0f0f
fabienmacip <fabien.macip@gmail.com>
2022-04-04 18:42:31 +0200

    CV du candidat READ et CREATE ok. Reste à pouvoir UPDATE

--- src/Controller/CandidatController.php
+++ src/Controller/CandidatController.php
@@ -7,6 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Candidat;
+use App\Entity\User;
+use App\Form\CandidatType;
+use App\Form\UserType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 
 class CandidatController extends AbstractController
@@ -63,15 +71,123 @@ class CandidatController extends AbstractController
 
 
     /**
-     * CREATE or UPDATE
+     * EDIT (UPDATE)
      * 
      * @Route("/candidat/update/{id}", name="candidat_update", requirements={"id"="\d+"})
      * @Route("/candidat/create/", name="candidat_create")
      */
-    public function edit(Candidat $candidat = null, Request $request): Response
+    public function edit(Candidat $candidat = null, User $user = null, Request $request): Response
     {
+        $em = $this->getDoctrine()->getManager();
+
+        // Savoir si on est en MODIFICATION (edit) ou AJOUT d'un candidat
+        $editMode = true;
+
+        if(!$candidat) {
+            $candidat = new Candidat();
+            $editMode = false;
+        }
+        
+        if(!$user) {
+            $user = $em->getRepository(User::class)->findOneBy(['id'=>$candidat->getUser()->getId()]);
+            $editMode = false;
+        } 
+        
+        if($request->request->get('formUser')) {
+            $user->setNom($request->request->get('formUser')['nom']);
+            $user->setPrenom($request->request->get('formUser')['prenom']);
+            $user->setUsername($request->request->get('formUser')['username']);
+            $user->setPassword($request->request->get('formUser')['password']);
+            $user->setRole($request->request->get('formUser')['role']);
+            
+        }
+        
+        $candidat->setUser($user);
+        
+        // Concaténation du chemin de téléchargement configuré avec le nom de fichier stocké et création d'une nouvelle classe File.
+        if($candidat->getCv() !== null){
+            
+            $candidat->setCv(
+                new File($this->getParameter('cv_directory').'/'.$candidat->getCv())
+            );
+        }
+        
+
+        // Champs du formulaire, partie USER
+        $formUser = $this->createForm(UserType::class, $user);
+        /* var_dump($formUser); */
+        
+        $formUser = $this->get('form.factory')->createNamedBuilder('formUser',UserType::class, $user)
+                        ->getForm();
+                    
+        
+                
+        // Champs du formulaire CANDIDAT (CV uniquement)
+         $form = $this->createFormBuilder($candidat)
+                    ->add('cv', FileType::class, array('data_class' => null))
+                    ->getForm();
+
+        $form->handleRequest($request);
+        $formUser->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            // Traitement du fichier PDF du CV
+            
+            // Récupération des données du formulaire
+            $file = $form['cv']->getData();
+
+            if($file && $file->guessExtension() === 'pdf'){
+
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                //$safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $safeFilename = "";
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                // Move the file to the directory where CV are stored
+                try {
+                $file->move(
+                    $this->getParameter('cv_directory'),
+                    $newFilename
+                );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'cvFilename' property to store the PDF file name
+                // instead of its contents
+                $candidat->setCv($newFilename);
+
+                $em->persist($candidat);
+                $em->flush();
+            } // FIN du IF($file)
+
+
             return $this->redirectToRoute('candidats');
-    } // FIN function create ou update
+            //return $this->render('candidat', ['id' => $candidat->getId()]);
+        } // FIN du IF formulaire CV validé
+
+        if($formUser->isSubmitted() && $formUser->isValid()) {
+            var_dump("valide user");
+                $em->persist($user);
+                $em->flush();
+    
+                return $this->redirectToRoute('candidats');
+                //return $this->render('candidat', ['id' => $candidat->getId()]);
+            } else if (!$formUser->isSubmitted()) {
+                var_dump("pas soumis");
+            }
+    
+
+
+        return $this->render('candidat/create.html.twig',[
+            'formCandidat' => $form->createView(),
+            'formUser' => $formUser->createView(),
+            'editMode' => $editMode
+        ]);
+
+
+    } // FIN function EDIT
 
 
 
--- src/Form/CandidatType.php
+++ src/Form/CandidatType.php
@@ -6,13 +6,38 @@ use App\Entity\Candidat;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
 
 class CandidatType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('cv')
+            ->add('cv', FileType::class, [
+                'label' => 'Brochure (PDF file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // everytime you edit the CV details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5000k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Fournissez svp un PDF valide',
+                    ])
+                ],
+            ])
             ->add('user')
             ->add('consultant')
         ;
